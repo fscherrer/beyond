d20db223b43ac99f823748e85c1f47a56cab48c3
lucasbotechia <lucasedub1@hotmail.com>
2013-04-13 01:05:48 -0300

    Modificado ícone, barra de título e tela inicial.
     - Alterado Ícone Android,
     - Removido barra de título,
    - Atualizado esquema de toque na tela inicial para entrar.

--- beyond-mobile/src/br/com/einsteinlimeira/beyond/mobile/InicialActivity.java
+++ beyond-mobile/src/br/com/einsteinlimeira/beyond/mobile/InicialActivity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import br.com.einsteinlimeira.beyond.mobile.services.EventoServices;
@@ -18,24 +19,30 @@ import br.com.einsteinlimeira.beyond.model.Evento;
 public class InicialActivity extends GlobalActivity {
 	private TextView textViewInformacoes;
 	private Button buttonTentarNovamente;
+	private ImageView imageViewLogo;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_inicial);
-
+		imageViewLogo = (ImageView) findViewById(R.id.inicial_Logo);
 		textViewInformacoes = (TextView) findViewById(R.id.inicial_textViewInformacoes);
 		buttonTentarNovamente = (Button) findViewById(R.id.inicial_buttonTentarNovamente);
 
-		buttonTentarNovamente.setOnClickListener(new OnClickListener() {
+		OnClickListener onClickListener = new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				new ObtemEventosAsyncTask().execute();
 			}
-		});
+		};
 
-		new ObtemEventosAsyncTask().execute();
+		imageViewLogo.setOnClickListener(onClickListener);
+		buttonTentarNovamente.setOnClickListener(onClickListener);
+		textViewInformacoes.setOnClickListener(onClickListener);
+		findViewById(R.id.inicial).setOnClickListener(onClickListener);
+
+		textViewInformacoes.setText(R.string.toque_inicial);
 	}
 
 	private class ObtemEventosAsyncTask extends
