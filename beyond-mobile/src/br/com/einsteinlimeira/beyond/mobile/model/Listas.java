0f335145340faf0881c4ff0ea8475e5e52237f84
Lucas Lamana <lucasoliveiralamana@gmail.com>
2013-05-30 13:59:52 -0300

    Criado a parte de serviço para cidades.

--- beyond-mobile/src/br/com/einsteinlimeira/beyond/mobile/model/Listas.java
+++ beyond-mobile/src/br/com/einsteinlimeira/beyond/mobile/model/Listas.java
@@ -59,12 +59,12 @@ public class Listas {
 		bandas.add(bandaSete);
 		bandas.add(bandaOito);
 		
-		Casa casaUm = new Casa(01, "Casa 1", "12345645000155", null, null, null, null, null, null);
-		Casa casaDois = new Casa(02, "Casa 2", "12345645000155", null, null, null, null, null, null);
-		Casa casaTres = new Casa(03, "Casa 3", "12345645000155", null, null, null, null, null, null);
-		Casa casaQuatro = new Casa(04, "Casa 4", "12345645000155", null, null, null, null, null, null);
-		Casa casaCinco = new Casa(05, "Casa 5", "12345645000155", null, null, null, null, null, null);
-		Casa casaSeis = new Casa(07, "Casa 6", "12345645000155", null, null, null, null, null, null);
+		Casa casaUm = new Casa();
+		Casa casaDois = new Casa();
+		Casa casaTres = new Casa();
+		Casa casaQuatro = new Casa();
+		Casa casaCinco = new Casa();
+		Casa casaSeis = new Casa();
 		
 		casas.add(casaUm);
 		casas.add(casaDois);
@@ -89,14 +89,14 @@ public class Listas {
 		
 		
 		try {
-			eventos.add(new Evento(1, "Evento 1", DateUtils.dateHourFormat.parse("08/02/2013 22:30"), 15d, null, bandasUm));
-			eventos.add(new Evento(2, "Evento 2", DateUtils.dateHourFormat.parse("08/03/2013 21:30"), 20d, null, bandasDois));
-			eventos.add(new Evento(3, "Evento 3", DateUtils.dateHourFormat.parse("08/04/2013 23:30"), 25d, null, bandasTres));
-			eventos.add(new Evento(4, "Evento 4", DateUtils.dateHourFormat.parse("08/05/2013 22:00"), 15d, null, bandasUm));
-			eventos.add(new Evento(5, "Evento 5", DateUtils.dateHourFormat.parse("08/06/2013 20:30"), 30d, null, bandasDois));
-			eventos.add(new Evento(6, "Evento 6", DateUtils.dateHourFormat.parse("08/07/2013 21:30"), 20d, null, bandasQuatro));
-			eventos.add(new Evento(7, "Evento 7", DateUtils.dateHourFormat.parse("08/08/2013 21:00"), 25d, null, bandasUm));
-			eventos.add(new Evento(8, "Evento 8", DateUtils.dateHourFormat.parse("08/09/2013 19:30"), 15d, null, bandasDois));
+			eventos.add(new Evento(1, "Evento 1", DateUtils.dateHourFormat.parse("08/02/2013 22:30"), 15d, null, bandasUm, null));
+			eventos.add(new Evento(2, "Evento 2", DateUtils.dateHourFormat.parse("08/03/2013 21:30"), 20d, null, bandasDois, null));
+			eventos.add(new Evento(3, "Evento 3", DateUtils.dateHourFormat.parse("08/04/2013 23:30"), 25d, null, bandasTres, null));
+			eventos.add(new Evento(4, "Evento 4", DateUtils.dateHourFormat.parse("08/05/2013 22:00"), 15d, null, bandasUm, null));
+			eventos.add(new Evento(5, "Evento 5", DateUtils.dateHourFormat.parse("08/06/2013 20:30"), 30d, null, bandasDois, null));
+			eventos.add(new Evento(6, "Evento 6", DateUtils.dateHourFormat.parse("08/07/2013 21:30"), 20d, null, bandasQuatro, null));
+			eventos.add(new Evento(7, "Evento 7", DateUtils.dateHourFormat.parse("08/08/2013 21:00"), 25d, null, bandasUm, null));
+			eventos.add(new Evento(8, "Evento 8", DateUtils.dateHourFormat.parse("08/09/2013 19:30"), 15d, null, bandasDois, null));
 			
 		} catch (ParseException e) {
 			e.printStackTrace();
